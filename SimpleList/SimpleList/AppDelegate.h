4e315c020a698b562cd3c307f28d6e73274fc616
antonyouseph <antony.k@foundingminds.com>
2015-01-16 23:37:09 +0530

    Project Structure
    Creation Of Project Structure

---
+++ SimpleList/SimpleList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleList
+//
+//  Created by antonyouseph on 1/16/15.
+//  Copyright (c) 2015 antonyouseph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
